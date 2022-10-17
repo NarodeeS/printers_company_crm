6727806fb16ce67ef97f3f11e8265bc3f993b394
George Stykalin <gstykalin@mail.ru>
2022-10-17 16:59:10 +0300

    Initial Commit

---
+++ printers-company-crm/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  printers-company-crm
+//
+//  Created by George Stykalin on 17.10.2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ printers-company-crm/printers_company_crmApp.swift
@@ -0,0 +1,17 @@
+//
+//  printers_company_crmApp.swift
+//  printers-company-crm
+//
+//  Created by George Stykalin on 17.10.2022.
+//
+
+import SwiftUI
+
+@main
+struct printers_company_crmApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
